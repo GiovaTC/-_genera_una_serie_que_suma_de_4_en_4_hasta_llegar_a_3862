b1cf819e83b93660a12752203663792f3686e047
gtapiero <giovanny.tapiero@pi.edu.co>
2025-11-27 10:13:11 -0500

    commit - FormSerie4 :.

--- src/main/java/com/example/formserie4/FormSerie4.java
+++ src/main/java/com/example/formserie4/FormSerie4.java
@@ -7,5 +7,34 @@ import java.sql.*;
 
 public class FormSerie4 extends JFrame  {
 
+    private JTextArea txtResultado;
+    private JButton btnCalcular, btnGuardar;
+    private JLabel lblEstado;
+    private long resultadoFinal = 0;
 
+    private final String connectionString = "jdbc:oracle:thin:@//localhost:1521/orcl";
+    private final String userDB = "system";
+    private final String passDB = "Tapiero123";
+
+    public FormSerie4() {
+        setTitle("Serie de 4 en 4 - Java + oracle");
+        setSize(450,350);
+        setLayout(new FlowLayout());
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        txtResultado = new JTextArea(8, 35);
+        txtResultado.setEditable(false);
+        btnCalcular = new JButton("Generar Serie");
+        btnGuardar = new JButton("Guardar en BD");
+        lblEstado = new JLabel("Estado: en espera ...");
+
+        add(new JLabel("Resultado Serie:"));
+        add(txtResultado);
+        add(btnCalcular);
+        add(btnGuardar);
+        add(lblEstado);
+
+        //btnCalcular.addActionListener(this::generarSerie);
+        //btnGuardar.addActionListener(this::guardarEnBD);
+    }
 }
