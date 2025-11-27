ba90dee5a6314529495fccf8c26efea985151f9e
gtapiero <giovanny.tapiero@pi.edu.co>
2025-11-27 09:48:41 -0500

    commit-FormSerie4 .

---
+++ src/main/java/com/example/formserie4/FormSerie4.java
@@ -0,0 +1,11 @@
+package com.example.formserie4;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.sql.*;
+
+public class FormSerie4 extends JFrame  {
+
+
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,6 +6,8 @@ module com.example.formserie4 {
     requires com.dlsc.formsfx;
     requires net.synedra.validatorfx;
     requires org.kordamp.bootstrapfx.core;
+    requires java.desktop;
+    requires java.sql;
 
     opens com.example.formserie4 to javafx.fxml;
     exports com.example.formserie4;
